9c3b4a2013634459860bd99483ff1640951e43b1
Elvio Pedroso Martinelli <emartinelli@protonmail.com>
2020-05-24 17:01:11 -0300

    Fix sqlite url

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite://' + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
